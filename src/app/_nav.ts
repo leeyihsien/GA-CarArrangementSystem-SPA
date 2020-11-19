50b51a2cec8bcb6de78a72f221987a815b64a54c
(SSB-129) Eddy.Lee <eddy.lee@ssbshoes.local>
2020-11-19 13:33:16 +0800

    test

--- src/app/_nav.ts
+++ src/app/_nav.ts
@@ -1,4 +1,5 @@
 import { INavData } from '@coreui/angular';
+import { Injectable } from '@angular/core';
 
 export const navItems: INavData[] = [
   {
@@ -223,29 +224,11 @@ export const navItems: INavData[] = [
       }
     ]
   },
+
   {
-    name: 'Disabled',
-    url: '/dashboard',
-    icon: 'icon-ban',
-    badge: {
-      variant: 'secondary',
-      text: 'NEW'
-    },
-    attributes: { disabled: true },
+    title: true,
+    name: 'A1. Car Service',
   },
-  {
-    name: 'Download CoreUI',
-    url: 'http://coreui.io/angular/',
-    icon: 'icon-cloud-download',
-    class: 'mt-auto',
-    variant: 'success',
-    attributes: { target: '_blank', rel: 'noopener' }
-  },
-  {
-    name: 'Try CoreUI PRO',
-    url: 'http://coreui.io/pro/angular/',
-    icon: 'icon-layers',
-    variant: 'danger',
-    attributes: { target: '_blank', rel: 'noopener' }
-  }
+
+
 ];
